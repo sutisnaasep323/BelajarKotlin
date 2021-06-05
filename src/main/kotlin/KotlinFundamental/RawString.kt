22396e78cd98936e6b1cda7ee7cd0415f0359bdc
yummicodes <yummicodes@gmail.com>
2021-06-05 20:54:51 +0700

    KotlinFundamental_RawString

---
+++ src/main/kotlin/KotlinFundamental/RawString.kt
@@ -0,0 +1,12 @@
+package KotlinFundamental
+
+fun main(){
+    var profile = """
+        Assalamu'alaikum. nama saya Asep
+        Saya berumur 19 tahun
+        Domisili Bogor
+        Pekerjaan Guru
+    """.trimIndent()
+
+    println(profile)
+}
